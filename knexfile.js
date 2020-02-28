0538cfd6db972bc2c9d0d9ced864025254611cdf
Jason Long <jlong5795@gmail.com>
2020-02-27 22:30:22 -0600

    Up to date

--- knexfile.js
+++ knexfile.js
@@ -50,16 +50,13 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory: './database/migrations',
       tableName: "knex_migrations"
     }
   }
